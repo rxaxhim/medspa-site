5e8ff08f0417cb098f38609e1a338fbc1d1ebc9e
Raahim Ghori <raahimghori@gmail.com>
2025-08-23 04:56:15 -0400

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite"
  
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/medspa-site/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
